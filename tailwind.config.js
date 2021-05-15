4ceb135eb5ecf72121b4f4b5921831267904a590
Lise <lise.carpenter@gmail.com>
2021-05-15 15:43:43 +0700

    configure typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			gray: colors.coolGray,
+			blue: colors.lightBlue,
+			red: colors.rose,
+			pink: colors.fuchsia,
+			green: {
+				500: '#4a7337',
+			},
+		},
+		fontFamily: {
+			sans: ['Lato', 'sans-serif'],
+		},
+		extend: {
+			spacing: {
+				128: '32rem',
+				144: '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+		},
 	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
 }
