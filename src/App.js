4020e09d6601d4999fadccb2dc34330ad66472a7
Lise <lise.carpenter@gmail.com>
2021-05-15 15:58:58 +0700

    styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css'
 function App() {
 	return (
 		<>
-			<h1 class='text-green-500 font-bold'>Carbonbase Login</h1>
+			<div className='wrapper bg-chalet-green flex items-center justify-center h-screen'>
+				<div className='left-section bg-chalet-green-100 h-screen w-1/2'>
+					&nbsp;
+				</div>
+				<div className='right-section bg-chalet-green w-1/2 h-screen flex items-center justify-center'>
+					<div className='text-wrap text-center'>
+						<h1 className='text-white font-bold text-5xl pb-4'>Carbonbase</h1>
+						<p className='text-white'>Let's stop the climate clock.</p>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
