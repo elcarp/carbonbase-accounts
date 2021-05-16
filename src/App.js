d00f2a22f8fac3643313b2255518fb5b8725a8da
Lise <lise.carpenter@gmail.com>
2021-05-16 12:45:52 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import Main from './layout/main'
 import Login from './layout/login.js'
 import React from 'react'
 import useToken from './hooks/useToken'
-
-function setToken(userToken) {
-	sessionStorage.setItem('token', JSON.stringify(userToken))
-}
-
-function getToken() {
-	const tokenString = sessionStorage.getItem('token')
-	const userToken = JSON.parse(tokenString)
-	return userToken?.token
-}
+// function setToken(userToken) {
+// 	sessionStorage.setItem('token', JSON.stringify(userToken))
+// }
+// function getToken() {
+// 	const tokenString = sessionStorage.getItem('token')
+// 	const userToken = JSON.parse(tokenString)
+// 	return userToken?.token
+// }
 
 function App() {
 	const { token, setToken } = useToken()
--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Dashboard from '../components/Dashboard'
-import Preferences from '../components/Preferences'
+import Dashboard from '../components/dashboard'
+import Preferences from '../components/preferences'
 
 function Main() {
 
