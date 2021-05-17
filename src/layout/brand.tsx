dd96d4c3215330e3b16bb5d148d76094226baf4f
Lise <lise.carpenter@gmail.com>
2021-05-17 17:31:17 +0700

    WIP add points

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -15,30 +15,31 @@ const BrandAdmin: FunctionComponent = () => {
 		getUsers()
 	}, [])
 	const currentUser = users && users[users.length - 1]
+
+	const username = currentUser && currentUser.username
+	const name = currentUser && currentUser.name
+    const password = currentUser && currentUser.password
 	const points = currentUser && currentUser.points
-	// async function updateUser(points: any) {
-	//     const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
-	//     return fetch(userUrl, {
-	//         method: 'PATCH',
-	//         headers: {
-	//             'Content-Type': 'application/json',
-	//         },
-	//         body: JSON.stringify(points + 50,)
-	//     }).then((data) => data.json())
-	// }
-	function updateUser(points: any) {
-		const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
-		axios
-			.put(userUrl, points)
-			.then((response) => setUsers({ points: response.data.points }))
+
+	async function updateUser() {
+	    const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
+	    return fetch(userUrl, {
+	        method: 'PUT',
+	        headers: {
+	            'Content-Type': 'application/json',
+	        },
+	        body: JSON.stringify({points: points + 50, name: name, username: username, password: password})
+	    }).then((data) => data.json())	
 	}
+
+
 	return (
 		<>
 			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
 				<div className='content text-center'>
 					<h1 className='pb-4'>Brand Admin</h1>
 					<a
-						onClick={() => updateUser(points+50)}
+						onClick={() => updateUser()}
 						className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Award <strong>user</strong> 50 points
 					</a>
