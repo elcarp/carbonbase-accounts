77700dc2bad04a257085c148945801466bc98432
Lise <lise.carpenter@gmail.com>
2021-05-17 15:48:29 +0700

    button styling

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -6,10 +6,10 @@ const BrandAdmin: FunctionComponent = () => {
 			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
 				<div className='content text-center'>
 					<h1>Brand Admin</h1>
-					<button className='clear block mx-auto my-10'>
+					<button className='clear block mx-auto my-10 shadow-md mt-6 uppercase font-bold bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Award <strong>user</strong> 50 points
 					</button>
-					<button className='clear block mx-auto my-10'>
+					<button className='clear block mx-auto my-10 shadow-md mt-6 uppercase font-bold bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Redeem 50 points from <strong>user</strong>{' '}
 					</button>
 				</div>
