c07ccff3a34b1c3d1711f2bfcc3b73fd7f4f85ba
Lise <lise.carpenter@gmail.com>
2021-05-17 15:44:30 +0700

    brand admin section UI

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -3,7 +3,9 @@ import React, { FunctionComponent } from 'react'
 const BrandAdmin: FunctionComponent = () => {
 	return (
 		<>
-			Brand Admin
+			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
+				Brand Admin
+			</div>
 		</>
 	)
 }
