426d416e726a478af56aeaac55486ce990a09476
Lise <lise.carpenter@gmail.com>
2021-05-17 15:47:22 +0700

    ui and styling

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -4,7 +4,15 @@ const BrandAdmin: FunctionComponent = () => {
 	return (
 		<>
 			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
-				Brand Admin
+				<div className='content text-center'>
+					<h1>Brand Admin</h1>
+					<button className='clear block mx-auto my-10'>
+						Award <strong>user</strong> 50 points
+					</button>
+					<button className='clear block mx-auto my-10'>
+						Redeem 50 points from <strong>user</strong>{' '}
+					</button>
+				</div>
 			</div>
 		</>
 	)
