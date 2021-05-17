aef46ab80b06382e973df3773d5a49ebb189dfd3
Lise <lise.carpenter@gmail.com>
2021-05-17 17:32:35 +0700

    redeem points

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -18,20 +18,40 @@ const BrandAdmin: FunctionComponent = () => {
 
 	const username = currentUser && currentUser.username
 	const name = currentUser && currentUser.name
-    const password = currentUser && currentUser.password
+	const password = currentUser && currentUser.password
 	const points = currentUser && currentUser.points
 
-	async function updateUser() {
-	    const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
-	    return fetch(userUrl, {
-	        method: 'PUT',
-	        headers: {
-	            'Content-Type': 'application/json',
-	        },
-	        body: JSON.stringify({points: points + 50, name: name, username: username, password: password})
-	    }).then((data) => data.json())	
+	async function addPoints() {
+		const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
+		return fetch(userUrl, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				points: points + 50,
+				name: name,
+				username: username,
+				password: password,
+			}),
+		}).then((data) => data.json())
 	}
 
+	async function redeemPoints() {
+		const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
+		return fetch(userUrl, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				points: points - 50,
+				name: name,
+				username: username,
+				password: password,
+			}),
+		}).then((data) => data.json())
+	}
 
 	return (
 		<>
@@ -39,21 +59,12 @@ const BrandAdmin: FunctionComponent = () => {
 				<div className='content text-center'>
 					<h1 className='pb-4'>Brand Admin</h1>
 					<a
-						onClick={() => updateUser()}
+						onClick={() => addPoints()}
 						className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Award <strong>user</strong> 50 points
 					</a>
-					{/* {users && addPointsMessage} */}
 					<a
-						onClick={() =>
-							currentUser &&
-							alert(
-								currentUser.name +
-									' now has ' +
-									(currentUser.points - 50) +
-									' points'
-							)
-						}
+						onClick={() => redeemPoints()}
 						className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Redeem 50 points from <strong>user</strong>
 					</a>
