991b84d2df9062bade4ee94560505d840fb655a2
Lise <lise.carpenter@gmail.com>
2021-05-17 16:36:55 +0700

    add axios

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -14,20 +14,39 @@ const BrandAdmin: FunctionComponent = () => {
 		getUsers()
 	}, [])
 	const currentUser = users && users[users.length - 1]
-	console.log(users && currentUser.points)
+
+    async function updateUser(points: any) {
+        const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
+        return fetch(userUrl, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(points + 50,)
+        }).then((data) => data.json())
+    }
 	return (
 		<>
 			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
 				<div className='content text-center'>
 					<h1 className='pb-4'>Brand Admin</h1>
 					<a
-						onClick={() => currentUser && alert(currentUser.name + ' now has ' + (currentUser.points + 50) + ' points')}
+						onClick={() => updateUser('soemthing')}
 						className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Award <strong>user</strong> 50 points
 					</a>
-					<a 
-                    onClick={() => currentUser && alert(currentUser.name + ' now has ' + (currentUser.points - 50) + ' points')}
-                    className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
+					{/* {users && addPointsMessage} */}
+					<a
+						onClick={() =>
+							currentUser &&
+							alert(
+								currentUser.name +
+									' now has ' +
+									(currentUser.points - 50) +
+									' points'
+							)
+						}
+						className='clear no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
 						Redeem 50 points from <strong>user</strong>
 					</a>
 				</div>
