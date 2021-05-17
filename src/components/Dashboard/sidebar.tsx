6043b96c3249caebce68a23551b1e02e53972291
Lise <lise.carpenter@gmail.com>
2021-05-17 08:19:29 +0700

    styling

--- src/components/Dashboard/sidebar.tsx
+++ src/components/Dashboard/sidebar.tsx
@@ -1,22 +1,34 @@
 import React, { FunctionComponent } from 'react'
+import { FaLeaf } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const SideBar: FunctionComponent = () => {
 	return (
 		<>
 			<div className='sidebar w-1/4 bg-chalet-green h-screen'>
-            <ul>
-					{/* <li className='logo'>
-						<a href='/'><img src={Burger} alt='logo'/></a>
-					</li> */}
+				<ul className='pl-3'>
 					<li>
-						<Link className='text-white no-underline' to='/dashboard'>Dashboard</Link>
+						<h1 className='text-white font-bold text-3xl py-10 pb-4 flex'>
+							Carbonbase <FaLeaf className='pl-4' />
+						</h1>
 					</li>
 					<li>
-						<Link className='text-white no-underline' to='/preferences'>Preferences</Link>
+						<Link className='text-white no-underline my-2 block' to='/'>
+							Offset
+						</Link>
+					</li>
+					<li>
+						<Link className='text-white no-underline my-2 block' to='/'>
+							Redeem
+						</Link>
+					</li>
+                    <li>
+						<Link className='text-white no-underline my-2 block' to='/'>
+							Learn
+						</Link>
 					</li>
 				</ul>
-            </div>
+			</div>
 		</>
 	)
 }
