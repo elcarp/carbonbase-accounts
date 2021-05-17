a06aacd80ad19cabe580ca657e253778e466a694
Lise <lise.carpenter@gmail.com>
2021-05-17 17:37:56 +0700

    show total users

--- src/layout/brand.tsx
+++ src/layout/brand.tsx
@@ -21,6 +21,8 @@ const BrandAdmin: FunctionComponent = () => {
 	const password = currentUser && currentUser.password
 	const points = currentUser && currentUser.points
 
+	const totalUsers = users && users.length
+	console.log('how many users?', totalUsers)
 	async function addPoints() {
 		const userUrl = users && 'http://localhost:3333/list/' + currentUser.id
 		return fetch(userUrl, {
@@ -58,6 +60,7 @@ const BrandAdmin: FunctionComponent = () => {
 			<div className='brand-section h-screen bg-chalet-green-600 flex justify-center items-center text-white'>
 				<div className='content text-center'>
 					<h1 className='pb-4'>Brand Admin</h1>
+					<p>{users && (<>There are currently <strong>{totalUsers}</strong> users</>)}</p>
 					<a
 						onClick={() => addPoints()}
 						className='clear cursor-pointer no-underline text-white block mx-auto my-10 shadow-md mt-6 uppercase bg-chalet-green-200 w-full text-lg px-5 py-2 rounded-lg uppercase hover:bg-chalet-green active:bg-chalet-green-100 focus:outline-none'>
